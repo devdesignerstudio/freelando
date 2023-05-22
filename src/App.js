d49ef70da66b656cc8492d34d8799a4219cecbf3
devdesignerstudio <devdesignerstudio@protonmail.com>
2023-05-22 00:47:13 -0300

    Criar um provedor para o tema; Consumir o tema em componente styled; Consumir o tema em componentes Globais, de estilos globais; Criar e exportar um componente puramente visual, sem comportamento: o Cabecalho.jsx.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Card } from "./components/Card/Card"
 import { Styles } from "./components/GlobalStyles/Styles"
+import { Header } from "./components/Header/Header"
 import { Theme } from "./components/Theme/Theme"
 
 function App() {
   return (
     <Theme>
+      <Header />
       <Styles />
       <Card>
         <h1>Freelando</h1>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled"
+
+export const Header = styled.header`
+  padding: ${(props) => props.theme.spacing.m};
+  background: ${(props) => props.theme.colors.primary.a};
+`
